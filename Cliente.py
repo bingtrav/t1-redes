0a8d5f39defaa900069cb18054888b744dd58fe8
Brayan <bsalas.0694@gmail.com>
2016-10-13 15:52:10 -0600

    Manejo de excepciones en Cliente

--- Cliente.py
+++ Cliente.py
@@ -40,7 +40,17 @@ while not is_File:
 		print "Archivo no encontrado"
 
 # Solicitar al usuario cual será el puerto de conexión con el intermediario
-intermediate_port = input("Puerto de conexión con el intermediario: ")
+is_Port = False
+while not is_Port:
+	intermediate_port = input("Puerto de conexión con el intermediario: ")
+	# Conecta el socket en el puerto cuando el servidor esté escuchando
+	try:
+		server_address = ('localhost', intermediate_port)
+		sock.connect(server_address)
+		print >>sys.stderr, 'Conectando a %s puerto %s' % server_address
+		is_Port = True
+	except:
+		print "Puerto de conexión inválido: Intente de nuevo."
 
 # Invita al usuario a seleccionar el modo que desee
 mode = raw_input("Ingrese el modo a ejecutar \n normal o debug: ")
@@ -59,18 +69,12 @@ time_out = input("Cuanto tiempo desea el Time-Out del ACK: ")
 print >>sys.stderr, 'Tiempo del Time-Out configurado a %i ms' % time_out
 time_out = time_out * 0.001
  
-# Conecta el socket en el puerto cuando el servidor esté escuchando
-server_address = ('localhost', intermediate_port)
-print >>sys.stderr, 'conectando a %s puerto %s' % server_address
-sock.connect(server_address)
-
-
 # Enviar datos del cliente hacia el servidor
 line = file_Open.readline()
 act_Id = 0
 leng = len(line)
 try:
-	sock.settimeout(0.1)
+	sock.settimeout(0.1)			# Se asigna un tiempo de espera de mensajes limitado 
 	# Llena la ventana inicial
 	for i in xrange(windowSR):
 		packet = str(act_Id)
@@ -95,6 +99,7 @@ try:
  	finish = False
 
 	while not finish:
+		# Revisa todos los ACKs recibidos.
 		noMsn = False
 		while not noMsn:
 			print "enter"
@@ -114,10 +119,12 @@ try:
 				print "enter no enter"
 				noMsn = True
 
+		# Si ya no hay más en la linea, saca la siguiente.
 		if leng == 0:
 			line = file_Open.readline()
 			leng = len(line)
 
+		# Revisa que la ventana contenga algo y luego si el primer paquete fue recibido para moverla y enviar otro.
 		while vecId and vecId[0] == -1:
 			vecWindow.pop(0)
 			vecTimer.pop(0)
@@ -154,6 +161,7 @@ try:
 					ventana += "|"
 				print "[debug] %s" % ventana
 
+		# Revisa cada paquete si hay time-out como si se revisaran todos al mismo tiempo.
 		t_a = time.time()
 		for act in xrange(len(vecWindow)):
 			if t_a >= vecTimer[act]:
@@ -164,6 +172,7 @@ try:
 				t = t_b + time_out
 				vecTimer[act] = t
 
+		# Si ya no hay nada en la ventana significa que ya no hay paquetes que enviar ni reenviar.
 		if not vecWindow:
 			finish = True
 
